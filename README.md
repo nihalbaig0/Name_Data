36b2f18c0dbd402bde99a0469d92ee4eaf47c439
Md. Babul Hasan (NoYoN) <bhyean16@cse.pstu.ac.bd>
2024-08-28 13:35:51 +0600

    psql

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# Data Annotation
+# Name Data Annotation
  
 
+```python
+CREATE DATABASE namedb;
+CREATE USER noyon WITH PASSWORD '1234';
+GRANT ALL PRIVILEGES ON DATABASE namedb TO noyon;
+```
--- annotation/settings.py
+++ annotation/settings.py
@@ -55,11 +55,22 @@ WSGI_APPLICATION = 'annotation.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'namedb',
+        'USER': 'noyon',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',  # Set to your PostgreSQL server host
+        'PORT': '5432',       # Default port for PostgreSQL
     }
 }
 
